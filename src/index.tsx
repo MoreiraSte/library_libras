6fcf8181b659c9bd571d97f8613d1e32c6732d13
Grande Stephany (CaP/ETS) <grs2ca@bosch.com>
2023-04-17 11:12:03 -0300

    url local - library libras

---
+++ src/index.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {libras} from './codePlugin.js'
+
+export default class VLibras extends Component<{ forceOnload?: boolean }> {
+  widgetSrc: string
+  scriptSrc: any
+  script: any
+
+  constructor(props: any) {
+    super(props)
+    this.widgetSrc = 'https://vlibras.gov.br/app'
+    this.scriptSrc = {libras}
+  }
+
+  init() {
+    this.script = document.createElement('script')
+    this.script.src = this.scriptSrc
+    this.script.async = true
+    this.script.onload = (load: any) => {
+      // @ts-ignore
+      new window.VLibras.Widget(this.widgetSrc)
+      if (this.props.forceOnload) {
+        // @ts-ignore
+        window.onload()
+      }
+    }
+    document.head.appendChild(this.script)
+  }
+
+  componentDidMount() {
+    this.init()
+  }
+
+  render() {
+    return (
+      // @ts-ignore
+      <div vw="true" className="enabled">
+        <div vw-access-button="true" className="active"></div>
+        <div vw-plugin-wrapper="true">
+          <div className="vw-plugin-top-wrapper"></div>
+        </div>
+      </div>
+    )
+  }
+}
